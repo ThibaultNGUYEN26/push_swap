d5006743fa7bd289708b06ead746234f52060105
Thibault <118262807+ThibaultNGUYEN26@users.noreply.github.com>
2023-02-20 21:12:45 +0100

    Update ft_rotate.c

--- push_swap/src/swap_moves/ft_rotate.c
+++ push_swap/src/swap_moves/ft_rotate.c
@@ -6,13 +6,13 @@
 /*   By: thibnguy <thibnguy@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/20 19:23:33 by thibnguy          #+#    #+#             */
-/*   Updated: 2023/02/20 19:48:30 by thibnguy         ###   ########.fr       */
+/*   Updated: 2023/02/20 21:07:15 by thibnguy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/push_swap.h"
 
-void	ft_rotate(t_node **stack)
+void	ft_rotate(t_node **stack, char *move)
 {
 	t_node	*tmp;
 	t_node	*last_node;
@@ -26,4 +26,5 @@ void	ft_rotate(t_node **stack)
 	last_node->next = tmp;
 	tmp->prev = last_node;
 	tmp->next = NULL;
+	ft_printf("%s\n", move);
 }
