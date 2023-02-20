cceff8bf7c1c50fc57c739adb25160c6a96afe8d
Thibault <118262807+ThibaultNGUYEN26@users.noreply.github.com>
2023-02-20 21:37:59 +0100

    Add files via upload

---
+++ push_swap/src/swap_moves/ft_rotate.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_rotate.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: thibnguy <thibnguy@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/20 19:23:33 by thibnguy          #+#    #+#             */
+/*   Updated: 2023/02/20 21:27:55 by thibnguy         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../includes/push_swap.h"
+
+void	ft_rotate(t_node **stack)
+{
+	t_node	*tmp;
+	t_node	*last_node;
+
+	tmp = *stack;
+	last_node = *stack;
+	while (last_node->next)
+		last_node = last_node->next;
+	*stack = (*stack)->next;
+	(*stack)->prev = NULL;
+	last_node->next = tmp;
+	tmp->prev = last_node;
+	tmp->next = NULL;
+}
