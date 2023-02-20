6d48df658761fca6a05adf11ea448f53a4ed0f43
Thibault <118262807+ThibaultNGUYEN26@users.noreply.github.com>
2023-02-20 21:30:28 +0100

    Add files via upload

---
+++ push_swap/src/push_swap.c
@@ -0,0 +1,106 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: thibnguy <thibnguy@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/20 17:14:20 by thibnguy          #+#    #+#             */
+/*   Updated: 2023/02/20 21:29:40 by thibnguy         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/push_swap.h"
+
+static void	*addlast_node(t_node **node_ref, int content)
+{
+	struct s_node	*new_node;
+	struct s_node	*current_node;
+
+	new_node = (t_node *) malloc(sizeof(t_node));
+	if (!new_node)
+		return (NULL);
+	new_node->content = content;
+	new_node->next = NULL;
+	if (!(*node_ref))
+	{
+		*node_ref = new_node;
+		return (new_node);
+	}
+	current_node = *node_ref;
+	while (current_node->next)
+		current_node = current_node->next;
+	current_node->next = new_node;
+	return (new_node);
+}
+
+static void	print_list(t_node *node, char *stack)
+{
+	ft_printf("%s : ", stack);
+	while (node != NULL)
+	{
+		ft_printf("%d ", node->content);
+		node = node->next;
+	}
+	printf("\n");
+}
+
+static void	free_list(t_node *node)
+{
+	t_node	*tmp;
+
+	while (node)
+	{
+		tmp = node;
+		node = node->next;
+		free(tmp);
+	}
+}
+
+int	main(int argc, char *argv[])
+{
+	t_node	*a;
+	t_node	*b;
+	int		i;
+
+	a = NULL;
+	b = NULL;
+	i = 0;
+	if (argc <= 2)
+	{
+		free_list(a);
+		free_list(b);
+		return (0);
+	}
+	if (argc > 1)
+	{
+		while (++i != argc)
+			addlast_node(&a, ft_atoi(argv[i]));
+		ft_printf("Linked list : ");
+		print_list(a, "Linked list");
+		print_list(a, "a");
+		print_list(b, "b");
+		ft_push(&b, &a);
+		ft_printf("PB\n");
+		print_list(a, "a");
+		print_list(b, "b");
+		ft_swap(&a);
+		ft_printf("SA\n");
+		print_list(a, "a");
+		print_list(b, "b");
+		ft_rotate(&a);
+		ft_printf("RA\n");
+		print_list(a, "a");
+		print_list(b, "b");
+		ft_push(&b, &a);
+		ft_printf("PB\n");
+		print_list(a, "a");
+		print_list(b, "b");
+		ft_ss(a, b);
+		print_list(a, "a");
+		print_list(b, "b");
+	}
+	free_list(a);
+	free_list(b);
+	return (0);
+}
---
+++ push_swap/src/swap_moves/ft_push.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_push.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: thibnguy <thibnguy@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/20 19:49:04 by thibnguy          #+#    #+#             */
+/*   Updated: 2023/02/20 21:27:45 by thibnguy         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../includes/push_swap.h"
+
+void	ft_push(t_node **stack_a, t_node **stack_b)
+{
+	t_node	*tmp;
+
+	if (*stack_b == NULL)
+		return ;
+	tmp = *stack_b;
+	*stack_b = (*stack_b)->next;
+	if (*stack_b)
+		(*stack_b)->prev = NULL;
+	tmp->next = *stack_a;
+	if (*stack_a)
+		(*stack_a)->prev = tmp;
+	tmp->prev = NULL;
+	*stack_a = tmp;
+}
