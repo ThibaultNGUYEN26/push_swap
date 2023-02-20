95b7317b7efbfa71ead3dda0d1893368ced2af00
Thibault <118262807+ThibaultNGUYEN26@users.noreply.github.com>
2023-02-20 21:12:09 +0100

    Update ft_swap.c

--- push_swap/src/swap_moves/ft_swap.c
+++ push_swap/src/swap_moves/ft_swap.c
@@ -6,17 +6,18 @@
 /*   By: thibnguy <thibnguy@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/20 17:49:40 by thibnguy          #+#    #+#             */
-/*   Updated: 2023/02/20 19:47:55 by thibnguy         ###   ########.fr       */
+/*   Updated: 2023/02/20 21:07:04 by thibnguy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/push_swap.h"
 
-void	ft_swap(t_node **stack)
+void	ft_swap(t_node **stack, char *move)
 {
 	int	tmp;
 
 	tmp = (*stack)->content;
 	(*stack)->content = (*stack)->next->content;
 	(*stack)->next->content = tmp;
+	ft_printf("%s\n", move);
 }
